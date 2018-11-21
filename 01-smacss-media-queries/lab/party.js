23d23328ad08070aa0587f4591449de8840b35ff
hoarous <semiramis.mei@gmail.com>
2018-11-20 20:47:41 -0800

    attempted to implement a color gradient generator. sometimes seems to work only on a portion of the divs? weird

--- 01-smacss-media-queries/lab/party.js
+++ 01-smacss-media-queries/lab/party.js
@@ -1,19 +1,40 @@
+const divIds = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i']
+
 var colorparty = function(event){
-    console.log('recoloring');
-    pickColor('a');
-    pickColor('b');
-    pickColor('c');
-    pickColor('d');
-    pickColor('e');
-    pickColor('f');
-    pickColor('g');
-    pickColor('h');
-    pickColor('i');
+    colorGrad();
 }
 
-var pickColor = function(divId){
-    let colorDiv = document.getElementById(divId);
-    let ranColor = Math.round(Math.random()*16777215);
-    let hexColor = 'background-color:#' + ranColor.toString(16);
-    colorDiv.setAttribute('style', hexColor);
+var pickColor = function(){
+    for(let i = 0; i < divIds.length; i++){
+        let colorDiv = document.getElementById(divIds[i]);
+        let ranColor = Math.round(Math.random()*16777215);
+        let hexColor = 'background-color:#' + ranColor.toString(16);
+        colorDiv.setAttribute('style', hexColor);
+    }
+}
+
+var colorGrad = function(){
+    let rGrad = [];
+    let gGrad = [];
+    let bGrad = [];
+
+    rGrad.push(Math.round(Math.random()*255));
+    gGrad.push(Math.round(Math.random()*255));
+    bGrad.push(Math.round(Math.random()*255));
+
+    let rStep = Math.floor((Math.round(Math.random()*255) - rGrad[0]) / divIds.length);
+    let gStep = Math.floor((Math.round(Math.random()*255) - rGrad[0]) / divIds.length);
+    let bStep = Math.floor((Math.round(Math.random()*255) - rGrad[0]) / divIds.length);
+
+    for(let i = 0; i < divIds.length - 1; i++){
+        rGrad.push(rGrad[i] + rStep);
+        gGrad.push(gGrad[i] + gStep);
+        bGrad.push(bGrad[i] + bStep);
+    }
+
+    for(let i = 0; i < divIds.length; i++){
+        let colorDiv = document.getElementById(divIds[i]);
+        let hexColor = 'background-color:#' + rGrad[i].toString(16) + gGrad[i].toString(16) + bGrad[i].toString(16);
+        colorDiv.setAttribute('style', hexColor);
+    }
 }
